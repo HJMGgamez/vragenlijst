a61478b3b062bee75c53ae92b7acb570cbfc48be
MSI\HJMGgamez <hermanmoret117@gmail.com>
2019-02-08 10:59:08 +0100

    eerste test vragelijst

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log("0");
+const eens = document.getElementById('eens');
+const oneens = document.getElementById('oneens');
+const geen = document.getElementById('geen');
+const statments = document.getElementById('statments');
+const statment = document.getElementById('statment');
+const question = document.getElementById('question');
+
+statment.innerHTML = subjects[0].title;
+question.innerHTML = subjects[0].statement;
