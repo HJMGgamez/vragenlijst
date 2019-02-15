44d2ff896c0106b70c3f5d23b76444ca73c26963
MSI\HJMGgamez <hermanmoret117@gmail.com>
2019-02-15 11:45:16 +0100

    display fixed

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 //var / const maken.
+const skip = document.getElementById('skip');
+const title = document.getElementById('title');
+const text = document.getElementById('text');
+const startBtn = document.getElementById('start_button');
 const proBtn = document.getElementById('pro');
 const ambivalentBtn = document.getElementById('ambivalent');
 const contraBtn = document.getElementById('contra');
-const statments = document.getElementById('statements');
 const statment = document.getElementById('statement');
 const question = document.getElementById('question');
 const back = document.getElementById('back');
+const check = document.getElementById('check');
+//I is waar ik mee bij hou waar ik ben met de vragen
 var i = 0;
 var answer = [];
 var results = [];
@@ -14,10 +19,17 @@ for (var s = 0; s < parties.length; s++) {
 	results[s] = {"name": parties[s].name, "points": 0};
 
 }
+//start de stemwijzer
+function startForm() {
+	setStatment();
+	hidestartpage();
+	showForm();
+}
 //plaats de de vraag en title in html. 
 function setStatment() {
 	statement.innerHTML = subjects[i].title;
 	question.innerHTML = subjects[i].statement;
+
 }
 //veranderd de vraag en houd bij waar je bent in met je vragen.
 function changeStatement(counting) {
@@ -35,7 +47,7 @@ function changeStatement(counting) {
 //vult je antwoord in al je er op clicked
 function addAnswer(Answer, Direction) {
 		answer[i] = Answer;
-		console.log(answer[i]);
+		console.log(answer);
 		changeStatement(Direction);
 	
 }
@@ -53,10 +65,34 @@ function checkresults() {
 			}
 		}
 	}
-// sorteerd de array op punten 
-results.sort(function(a,b) {
-    return b["points"]-a["points"]
-});
+	// sorteerd de array op punten 
+	results.sort(function(a,b) {
+	    return b["points"]-a["points"]
+	});
+	console.log(results);
 }
-
-setStatment();
+//hide the start button
+function hidestartpage() {
+	startBtn.style.display = 'none';
+	text.style.display = 'none';
+	title.style.display = 'none';
+}
+function showForm() {
+	proBtn.style.display = '';
+	ambivalentBtn.style.display = '';
+	contraBtn.style.display = '';
+ 	statment.style.display = '';
+	question.style.display = '';
+	back.style.display = '';
+	skip.style.display = '';
+}
+function hideForm() {
+	proBtn.style.display = 'none';
+	ambivalentBtn.style.display = 'none';
+	contraBtn.style.display = 'none';
+ 	statment.style.display = 'none';
+	question.style.display = 'none';
+	back.style.display = 'none';
+	skip.style.display = 'none';
+	check.style.display = '';
+}
